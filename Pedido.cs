fae9d4ed812c1f80ecaa309dd39e29b5528f05a7
sophia <sophai@aluno.senai.br>
2022-11-18 15:52:22 -0300

    Juridico

---
+++ Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juridico
+{
+    public class Pedido
+    {
+        public int Numero { get; set; }
+        public double ValorPedido { get; set; }
+        public string DescricaoPedido { get; set; }
+        
+
+        public double getValorPedidosGlobal()
+        {
+            return 2;
+        }
+        public double getQuantidadeGlobal ()
+        {
+            return 2;
+        }
+    }
+}
---
+++ Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juridico
+{
+    public class Pessoa
+    {
+        public int Numero { get; set; }
+        public char TipoPessoa { get; set; }//F = Física, J = Jurídica
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+
+        public List<Pessoa> listaJuridica()
+        {
+            return null;
+        }
+        public List<Pessoa> listaFisica()
+        {
+            return null;
+        }
+    }
+}
