e132d0fe77f88335491082ca1ff3c61af66f58e6
sophia <sophai@aluno.senai.br>
2022-11-29 10:07:20 -0300

    atualizado1

---
+++ AdvocaciaSolution/AdvocaciaPrincipal/Modelos/Advogado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvocaciaPrincipal.Modelos
+{
+    public class Advogad
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public char Sexo { get; set; }
+
+        public bool verificarSeAdvogadoEMaiorIdade()
+        {
+            if (this.Idade >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+            public List<Advogado> getALLAdvogados()
+            {
+                return null;
+            }
+            public bool eExperiente()
+            {
+                return false;
+            }
+        }
+    } 
---
+++ AdvocaciaSolution/AdvocaciaPrincipal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvocaciaPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meu nome é Ícaro!");
+        }
+    }
+}
