3bb1860849423d9ff521692ce1b8af258c6cc130
sophia <115712101+icaro1300@users.noreply.github.com>
2022-12-01 12:20:07 -0300

    c

---
+++ AdvocaciaSolution/AdvocaciaPrincipal/Util/TratamentoDeTexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvocaciaPrincipal.Util
+{
+    public class TratamentoDeTexto
+    {
+        public int gtDeTexto(string texto)
+        {
+          int x = texto.Count();
+            return x;
+        }
+
+        public string TextoAlterado(string encontrar, string novo,string texto)
+        {
+            return texto.Replace(encontrar, novo);
+        }
+    }
+}
